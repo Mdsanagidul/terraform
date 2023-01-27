db9590bdf290ad58c37e5e0804abd97176159bda
user <user@terraform-server.fios-router.home>
2023-01-27 12:27:49 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  description = "AWS-EC2-OS-Image"
+  type        = string
+}
+variable "instance_type" {
+  description = "AWS-EC2-Instance-type"
+  type        = string
+}
+variable "name_tag" {
+  description = "AWS-EC2-Server-Name"
+  type        = string
+}
